4f9a9ffe73d6da6a3e7a7aad6d67309bfba9057d
Josh Cox <josh@webhosting.coop>
2015-05-06 18:08:45 -0500

    added splash and some more files to 7

---
+++ 7-gui/colorswap.gd
@@ -0,0 +1,25 @@
+
+extends Control
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+var tapped=false
+
+func _draw():
+	var r = Rect2( Vector2(), get_size() )
+	if (tapped):
+		draw_rect(r, Color(1,0,0) )
+	else:
+		draw_rect(r, Color(0,0,1) )
+
+func _input_event(ev):
+	if(ev.type==InputEvent.MOUSE_BUTTON and ev.pressed):
+		tapped=true
+		update()
+
+func _ready():
+	# Initalization here
+	pass
+
+
